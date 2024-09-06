5e34c4a0a9639e07f04c94e55182f9592ac53a47
Dylan Cheong Nam Kwong <dylancnk_97@hotmail.com>
2024-09-06 12:16:15 -0400

    check

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 (function () {
-    const ConversationType = window.unblu.ConversationType;
     window.unblu.floating.api.initialize().then(api => {
         // chat
         const chatBtn = document.getElementById('conversation');
-        chatBtn.addEventListener('click', () => api.getActiveConversation());
+        chatBtn.addEventListener('click', () => api.isInitialized());
     });
 })();
