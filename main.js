7af9b4bd6d73c798caa259844311c74ac1441ca7
Dylan Cheong Nam Kwong <dylancnk_97@hotmail.com>
2024-09-06 13:17:03 -0400

    kujughsd

--- main.js
+++ main.js
@@ -3,5 +3,11 @@
     window.unblu.floating.api.initialize().then(api => {
         const chatBtn = document.getElementById('start-conversation');
         chatBtn.addEventListener('click', () => api.startConversation(ConversationType.OFFLINE_CHAT_REQUEST));
+
+        const pinBtn = document.getElementById('join');
+        const pinInput = document.getElementById('pin-input');
+        pinBtn.addEventListener('click', () => {
+            api.joinConversation(pinInput.value).then(conversation => {
+                console.log('Joined the conversation: ', conversation.getConversationId());
     });
 })();
