643ec9b4c5b51b9b2a3e3ebc0c8e29ea57189fb5
Dylan Cheong Nam Kwong <dylancnk_97@hotmail.com>
2024-09-06 03:36:06 -0400

    mod5

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import { api } from "@unblu/floating-js-api";
+/// <reference path="typings/floating-api.d.ts" />
 
 (function () {
     const ConversationType = window.unblu.ConversationType;
-    api.initialize().then(api => {
+    window.unblu.api.initialize().then(api => {
         // chat
         const chatBtn = document.getElementById('start-chat');
         chatBtn.addEventListener('click', () => api.startConversation(ConversationType.CHAT_REQUEST));
