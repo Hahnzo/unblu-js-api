84f9b957f99004f8d1ddf73f989370c118c2c686
Dylan Cheong Nam Kwong <dylancnk_97@hotmail.com>
2024-09-06 11:48:14 -0400

    mod17

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-import { api } from "@unblu/floating-js-api";
+(function () {
+    const ConversationType = window.unblu.ConversationType;
+    window.unblu.floating.api.initialize().then(api => {
+        // chat
+        const chatBtn = document.getElementById('start-chat');
+        chatBtn.addEventListener('click', () => api.startConversation(ConversationType.CHAT_REQUEST));
+        // audio
+        const audioBtn = document.getElementById('start-audio-call');
+        audioBtn.addEventListener('click', () => api.startConversation(ConversationType.AUDIO_REQUEST));
+        // video
+        const videoBtn = document.getElementById('start-video-call');
+        videoBtn.addEventListener('click', () => api.startConversation(ConversationType.VIDEO_REQUEST));
+        // PIN
+        const pinBtn = document.getElementById('join');
+        const pinInput = document.getElementById('pin-entry');
+        pinBtn.addEventListener('click', () => {
+            api.joinConversation(pinInput.value).then(conversation => {
+                console.log('Joined conversation: ', conversation.getConversationId());
 
-const unbluApi = await api.initialize();
-
-const chatBtn = document.getElementById('start-chat');
-chatBtn.addEventListener('click', () => unbluApi.getActiveConversation());
+                // reset the value
+                pinInput.value = '';
+            });
+        });
+    });
+})();
